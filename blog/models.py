93a1f822e9bbf897c2257667eeec922e3aee7ef0
Gertrudekapplan <easppolivanova@gmail.com>
2023-02-02 14:02:53 +0300

    First commit!!!

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+
+
+    def __str__(self):
+        return self.title
